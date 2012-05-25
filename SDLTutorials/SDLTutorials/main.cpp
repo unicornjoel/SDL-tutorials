49ccc1fc41c34246ab85c4de57d81f139bb82056
Joel Nielsen <joel.dot.nielsen@gmail.com>
2012-05-25 00:42:51 -0400

    Tutorial 2 complete and working: hello world function. Loads bitmap files, blits them to a screen, updates the screen and waits for two seconds to ensure that it is visibly working. Not the best way to leave the screen visible, that will be shown in tutorial 4.

--- SDLTutorials/SDLTutorials/main.cpp
+++ SDLTutorials/SDLTutorials/main.cpp
@@ -1,10 +1,33 @@
+//Include the SDL functions and datatypes
 #include "SDL.h"
 
 int main( int argc, char* args[] )
 {
+	//The images
+	SDL_Surface* hello = NULL;
+	SDL_Surface* screen = NULL;
+
     //Start SDL
-    SDL_Init( SDL_INIT_EVERYTHING );
-    
+    SDL_Init ( SDL_INIT_EVERYTHING );
+
+	//Set up the screen
+	screen = SDL_SetVideoMode ( 640, 480, 32, SDL_SWSURFACE );
+
+	//Load image
+	hello = SDL_LoadBMP ( "hello.bmp" );
+
+	//Apply image to the screen
+	SDL_BlitSurface ( hello, NULL, screen, NULL );
+
+	//Update screen
+	SDL_Flip ( screen );
+
+	//Pause
+	SDL_Delay ( 2000 );
+
+	//Free the loaded image
+	SDL_FreeSurface ( hello );
+
     //Quit SDL
     SDL_Quit();
     
