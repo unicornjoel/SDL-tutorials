6e14756a9b3c5ce125fd72868859918c3e8af22b
Joel Nielsen <joel.dot.nielsen@gmail.com>
2012-05-27 00:32:19 -0400

    This commit is tutorial #2. It contains a new function for loading and optimizing images, one for blitting and a method to set the window caption. It also contains the first sample of blitting the same image multiple times, and commands to end the program if it encounters problems.

--- SDLTutorials/SDLTutorials/main.cpp
+++ SDLTutorials/SDLTutorials/main.cpp
@@ -1,32 +1,102 @@
 //Include the SDL functions and datatypes
 #include "SDL.h"
+#include <string>
+
+//Attributes for the screen
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+const int SCREEN_BPP = 32;
+
+//Surfaces to be used in the program
+SDL_Surface *message = NULL;
+SDL_Surface *background = NULL;
+SDL_Surface *screen = NULL;
+
+SDL_Surface *load_image( std::string filename )
+{
+	//Temporary storage for the string that will be loaded
+	SDL_Surface *loadedImage = NULL;
+
+	//The optimized image that will be used
+	SDL_Surface *optimizedImage = NULL;
+
+	//Load the image
+	loadedImage = SDL_LoadBMP( filename.c_str() );
+
+	//If nothing went wrong
+	if( loadedImage != NULL )
+	{
+		//Create an optimized image
+		optimizedImage = SDL_DisplayFormat( loadedImage );
+
+		//Free the old surface
+		SDL_FreeSurface( loadedImage );
+	}
+
+	//Return the optimized image
+	return optimizedImage;
+}
+
+void apply_surface( int x, int y, SDL_Surface* source, SDL_Surface* destination)
+{
+	//Make a temporary rectancle to hold the offsets
+	SDL_Rect offset;
+
+	//Give the offsets to the rectangle
+	offset.x = x;
+	offset.y = y;
+
+	//Blit the surface
+	SDL_BlitSurface( source, NULL, destination, &offset );
+}
+
 
 int main( int argc, char* args[] )
 {
-	//The images
-	SDL_Surface* hello = NULL;
-	SDL_Surface* screen = NULL;
 
-    //Start SDL
-    SDL_Init ( SDL_INIT_EVERYTHING );
+	//Initialize all SDL subsystems
+    if( SDL_Init ( SDL_INIT_EVERYTHING ) == -1 )
+	{
+		return 1;
+	}
 
 	//Set up the screen
-	screen = SDL_SetVideoMode ( 640, 480, 32, SDL_SWSURFACE );
+	screen = SDL_SetVideoMode ( SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_BPP, SDL_SWSURFACE );
 
-	//Load image
-	hello = SDL_LoadBMP ( "hello.bmp" );
+	//If there was an error in setting up the screen
+	if( screen==NULL )
+	{
+		return 1;
+	}
 
-	//Apply image to the screen
-	SDL_BlitSurface ( hello, NULL, screen, NULL );
+	//Set window caption
+	SDL_WM_SetCaption( "Hello World!", NULL );
+
+	//Load images
+	message = load_image( "hello.bmp" );
+	background = load_image( "background.bmp" );
+
+	//Apply the background to the screen
+	apply_surface( 0, 0, background, screen );
+	apply_surface( 320, 0, background, screen );
+	apply_surface( 0, 240, background, screen );
+	apply_surface( 320, 240, background, screen );
+
+	//Apply the message to the screen
+	apply_surface( 180, 140, message, screen );
 
 	//Update screen
-	SDL_Flip ( screen );
+	if( SDL_Flip ( screen ) == -1 )
+	{
+		return 1;
+	}
 
-	//Pause
+	//Wait 2 seconds
 	SDL_Delay ( 2000 );
 
-	//Free the loaded image
-	SDL_FreeSurface ( hello );
+	//Free the surfaces
+	SDL_FreeSurface ( message );
+	SDL_FreeSurface ( background );
 
     //Quit SDL
     SDL_Quit();
